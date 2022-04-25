a802ac33176181ad1cc5dac14d238755ae4d83a0
J1u2a3n9 <juan.canedo.v@ucb.edu.bo>
2022-04-25 19:05:43 -0400

    Basic interface Added
    Co-authored-by: KarolZerna <karol.zerna@gmail.com>
    
    Co-authored-by: MercedesLaFuente <mercedes.htcv@gmail.com>
    
    Co-authored-by: ASDIO1 <diego.an.sanchez.a@gmail.com>
    
    Co-authored-by: mabatule1998 <maba1998@gmail.com>

---
+++ AppCleanDataBase/app/src/main/java/ucb/edu/bo/appcleandatabase/AddBooks.kt
@@ -0,0 +1,11 @@
+package ucb.edu.bo.appcleandatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_books)
+    }
+}
---
+++ AppCleanDataBase/app/src/main/java/ucb/edu/bo/appcleandatabase/Books.kt
@@ -0,0 +1,11 @@
+package ucb.edu.bo.appcleandatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Books : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
